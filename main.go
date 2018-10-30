6754a1b16eb752bf715081c5502a0f14e27f3ac4
gokaykucuk <serdargokaykucuk@gmail.com>
2018-10-30 23:20:50 +0100

    Programmers are in a race with the Universe to create bigger and better idiot-proof programs, while the Universe is trying to create bigger and better idiots. So far the Universe is winning.|Rich Cook

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	splitted_message := strings.Split(chosen_message, "|")
 	color.Green("Adding and commiting your files with commit message...\n")
 	fmt.Println(splitted_message[0]+"\n")
-	fmt.Println(color.New(color.Bold, color.BgCyan, color.Italic).Sprint("   " + splitted_message[1]+ "   "))
+	(color.New(color.Bold, color.BgCyan, color.Italic).Print("   " + splitted_message[1]+ "   ")
 	// I like how it does warn you..
 	commit_cmd := exec.Command("git", "commit", "-a", "-m", getRandomQuote())
 	commit_cmd.Run()
@@ -38,7 +38,6 @@ func main() {
 		push_cmd := exec.Command("git", "push")
 		push_cmd.Run()
 	}
-	fmt.Print("\n")
 	color.New(color.BlinkSlow,color.FgRed).Println("!!! Please do this as rarely as possible !!!")
 }
 
